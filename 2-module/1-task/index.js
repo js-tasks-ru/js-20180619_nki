3264a270e3600eb983643933e269144be308ccb6
Nikolai <nlv.nki@gmail.com>
2018-06-29 19:04:25 +0300

    Задача решена

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,30 @@
  * @returns {Object}
  */
 function clone (obj) {
+
+  let newObj = {};
+
+  for (let key in obj) {
+    if (obj[key] !== null) {
+
+      if (typeof obj[key] === 'object') {
+        newObj[key] = clone(obj[key])
+      }
+
+      else {
+
+        newObj[key] = obj[key];
+
+      }
+    }
+    else {
+
+      newObj[key] = null
+
+    }
+  }
+
+
+return newObj;
+
 }
