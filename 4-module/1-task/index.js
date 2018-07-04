9788cbffbddacf6d6dbcf99342744216ebc5dce0
Nikolai <nlv.nki@gmail.com>
2018-07-04 12:38:58 +0300

    module4-task1 complite

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,6 +4,26 @@
  * Генерация HTML списка друзей
  * @param {Friend[]} friends
  * @return {HTMLUListElement}
+ *
+ *
  */
+
+
+
 function makeFriendsList (friends) {
+
+    let ul = document.createElement('ul');
+
+    friends.forEach((item, index, array) => {
+
+        let li = document.createElement('li');
+        li.innerHTML = item.firstName + ' ' + item.lastName;
+        ul.append(li);
+
+    });
+
+    return ul;
+
+
 }
+
