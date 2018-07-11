339bd7fbe5b401d99bc161884a8b020ae2d85789
Nikolai <nlv.nki@gmail.com>
2018-07-11 21:35:52 +0300

    fix #11191d7

---
+++ 5-module/1-task/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * Метод устанавливает необходимые по условию аттрибуты таблице
+ * @param {Element} table
+ */
+function highlight(table) {
+
+    let trs = table.querySelectorAll('tbody tr');
+
+    trs.forEach(function (item, index, array) {
+
+        let age = item.getElementsByTagName('td')[1];
+        let gender = item.getElementsByTagName('td')[2];
+        let status = item.getElementsByTagName('td')[3];
+
+        if (status.dataset.available === 'true') {
+
+            status.parentElement.classList.add('available');
+        }
+
+        else if (status.dataset.available === 'false' ) {
+
+            status.parentElement.classList.add('unavailable');
+        }
+
+        else  {
+
+            status.parentElement.setAttribute("hidden", "hidden");
+        }
+
+        if (gender.innerHTML === 'm') {
+
+            gender.parentElement.classList.add('male');
+        }
+
+        if (gender.innerHTML === 'f') {
+
+            gender.parentElement.classList.add('female');
+        }
+
+        if (age.innerHTML  < 18) {
+
+            age.parentElement.style.textDecoration = "line-through";
+        }
+
+    });
+
+}
